5fe7f62ac3dd86c794b2c4cd73ef5c21a7288176
mknadler <miriam.k.nadler@gmail.com>
2023-08-03 12:29:46 -0400

    Uppercase 'i' in main page body

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { siteTitle } from '$lib/config';
 	import Nervous from '$lib/components/Nervous.svelte'
+	import Paper from '$lib/components/Paper.svelte'
 
 	export let data;
 
@@ -11,10 +12,13 @@
 	<title>{siteTitle}</title>
 </svelte:head>
 
+<Paper />
+
+
 <Nervous />
 
 <p>My name is <strong>miri</strong>am nadler<strong>.</strong> Welcome to my web<strong>page</strong>.</p>
 
-<p>I make <a href="/work">things</a> with my hands, like books and boxes. I make things with code, like websites and animations and other tchotchkes. The things i make are usually mostly visual in nature because my brain is happiest when it's looking at something complicated.</p>
+<p>I make <a href="/work">things</a> with my hands, like books and boxes. I make things with code, like websites and animations and other tchotchkes. The things I make are usually mostly visual in nature because my brain is happiest when it's looking at something complicated.</p>
 
 <p>You can check out the <a href="https://github.com/mknadler/miri-page">GitHub repo</a> for this site, if you'd like.  I'm also currently looking <a href="/contact">for work</a>.</p>
