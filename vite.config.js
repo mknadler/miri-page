c452440ea4596dbda8a6a43e0b9b9573efa30fba
mknadler <miriam.k.nadler@gmail.com>
2023-08-03 11:52:10 -0400

    Copy images into static/images/crushed, append -crushed to filename, harshly compress them so I can use them for lazyloading later

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { ViteImageOptimizer as viteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		viteImageOptimizer({
+			exclude: /-crushed\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+			png: {
+				quality: 100
+			}
+		}),
+		viteImageOptimizer({
+			test: /-crushed\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+			png: {
+				quality: 1
+			}
+		})
+	],
 	server: {
 		fs: {
 			allow: ['.']
