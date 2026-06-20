e7e8e464e459c092424e57eaf7e9efa14007071f
mknadler <miriam.k.nadler@gmail.com>
2026-06-20 10:42:20 -0400

    Update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import '$lib/styles/style.css';
 
 	let { children } = $props();
