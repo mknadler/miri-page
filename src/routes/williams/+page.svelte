fce04c62efac96542d3c41f84045d89d79a98745
mknadler <miriam.k.nadler@gmail.com>
2026-07-08 19:49:57 -0400

    Add /williams page

---
+++ src/routes/williams/+page.svelte
@@ -0,0 +1,166 @@
+<script>
+	import Game from "./Game.svelte";
+</script>
+
+<article>
+	<header><h1>Oops! All Williams</h1></header>
+
+	<section class="meta">
+		<a href="https://app.matchplay.events/tournaments/252793">Matchplay URL</a>
+		<p>Qualifying: Friday 5pm to Midnight and Saturday 1pm to Midnight</p>
+	</section>
+
+	<Game name="Cactus Canyon (Lyman Upgrade)">
+		<p>Short version: gold mine multiball if you can, otherwise try to start all modes, otherwise go for quick draw</p>
+		<p><a href="https://tiltforums.com/t/cactus-canyon-remake-lyman-sheats-complete-upgrade-rulesheet/10018">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=yjuNbxOo0NE">Brief video</a></li>
+			<li><a href="https://www.youtube.com/watch?v=-sCym6l2FVM">Comprehensive</a></li>
+		</ul>
+	</Game>
+	<Game name="Congo">
+		<p>Short version: get diamonds to light locks, JP is left ramp (or far right ramp, which is harder). Shoot GRAY to do inner pf monkey stuff. Second completion starts a MB right away, if you have balls locked when you do they'll be "wasted". Top lanes for AMY is a great hurryup. AVOID the very center targets.
+		<p><a href="https://pinball.org/rules/congo.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=gPTmUoTrvu4">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Creature from the Black Lagoon">
+		<p>Short version: FILM for MB, during MB try to shoot left ramp to build PF X. If you can safely do it, keep on hitting right ramp (ideally from a backhand in a way that safely returns to right flipper). Center over and over for Move Your Car; if your hurryup value gets v low consider just timing it out, because if you do the next start of MYC will only be a few shots instead of 6-10 or w/e</p>
+		<p><a href="https://www.pinball.org/rules/creaturefromtheblacklagoon.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=x_hJC1wpE_c">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Diner">
+		<p>Will fill this one in later after I fucking learn Diner</p>
+		<p><a href="https://pinball.org/rules/diner.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=v2kPwKvkNi4">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Funhouse">
+		<p>Short version: Soft plunge!!!. Multiball points are $$$$$. Get there by shooting lit arrow shots (ideally center in between Rudy and the scoop). When possible shoot Rudy from top left flipper. Worth watching the Bowen vid on this.</p>
+		<p><a href="https://pinball.org/rules/funhouse.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=wI8Z6R1ss8I">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Indiana Jones: The Pinball Adventure">
+		<p>Unfortunately this is sort of a knowledge check game; read the rulesheet. One thing: shooting the middle 3 targets down for locks. Also, mode start is the left saucer. Modes are lucrative. Also, the ball will come back (or just be returned to the PF from some shots far away) in a way that looks like it's not coming back on the right side, so be alert.</p>
+		<p><a href="https://www.pinball.org/rules/indianajones.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=u2G0A-BIpAY">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Johnny Mnemonic">
+		<p>Video mode is a LOT of points if you can get your head around the snarf mode controls. You've played enough video games that I assume you can figure that out. Besides that, main MB is good points, but watch the not bowen video for a good example of the other main strat (spinner millions). If you're completely lost, just ramp out.</p>
+		<p><a href="https://pinball.org/rules/johnnymnemonic.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=TRbjJeIpLoA">Not Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Medieval Madness">
+		<p>My personal strat depends on the return from the middle. If it returns in a way where you can safely trap up on the left flipper on the return I just hit castle all day. If the castle is risky I rush MB and get castles down during MB. For this format it'll be different: you'll want to get probably 40m+ to have a really solid score. That means "I win because I got 3 castles" like in matchplay won't cut it. Read up on the Royal Madness mechanic.</p>
+		<p><a href="https://www.pinball.org/rules/medievalmadness.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=6wb_x3q3z14">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Scared Stiff">
+		<p>Filling this in later when I remember</p>
+		<p><a href="https://pinball.org/rules/scaredstiff.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=RIf9Hn8dXsE">Non-Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Theatre of Magic">
+		<p>Filling this in later</p>
+		<p><a href="https://pinball.org/rules/theatreofmagic.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=BzLXJMNdoXs">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="Twilight Zone">
+		<p>Filling this in later</p>
+		<p><a href="https://pinball.org/rules/twilightzone.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=DYy0aNj9GLU">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="White Water">
+		<p>Filling this in later</p>
+		<p><a href="https://pinball.org/rules/whitewater.html">Rulesheet</a></p>
+		<p><a href="https://www.kineticist.com/news/white-water-pinball-tutorial">Kineticist guide</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=9zVJLyIf-F8">Bowen</a></li>
+		</ul>
+	</Game>
+	<Game name="World Cup Soccer">
+		<p>Filling this in later</p>
+		<p><a href="https://pinball.org/rules/worldcup.html">Rulesheet</a></p>
+		<span>Video tutorials</span>
+		<ul class="videos">
+			<li><a href="https://www.youtube.com/watch?v=YlXFzn4uzGQ">Bowen</a></li>
+		</ul>
+	</Game>
+</article>
+
+<style>
+	.meta a {
+		text-decoration: revert;
+		text-decoration-color: #ff006699;
+		text-decoration-style: solid;
+		text-decoration-skip-ink: auto;
+		text-decoration-thickness: 2px;
+		background: none;
+	}
+
+	h1 {
+		font-size: 28px;
+	}
+	h3 {
+		font-size: 24px;
+	}
+
+	.meta {
+		margin: 1rem 0 2.5rem 0;
+		padding-bottom: 2rem;
+		border-bottom: 1px dotted #ccc;
+		border-bottom-width: 2px;
+		p, a {
+			font-size: 20px;
+			color: #aaa;
+		}
+	}
+
+	article {
+		padding: 20px;
+		color: #ddd;
+	}
+
+	ul li {
+		margin-left: 2rem;
+	}
+	a {
+		color: #ddd;
+		text-decoration: none;
+		background: #6600ff;
+	}
+
+	p + p {
+		margin-top: 1rem;
+	}
+</style>
---
+++ src/routes/williams/Game.svelte
@@ -0,0 +1,24 @@
+<script>
+	let { name, children } = $props();
+</script>
+
+<section>
+	<h2>{name}</h2>
+	{@render children?.()}
+</section>
+
+<style>
+	h2 {
+		font-size: 24px;
+		font-weight: 600;
+		position: sticky;
+		top: 0;
+		background: black;
+		padding: 1rem 0;
+		margin-bottom: 8px;
+	}
+	section {
+		margin: 40px 0;
+		background: black;
+	}
+</style>
